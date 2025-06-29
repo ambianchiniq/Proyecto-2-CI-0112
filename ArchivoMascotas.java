3f47c14aee54798cc8eea9efaf5d14b4f5f8d6eb
ambianchiniq <alejandro.bianchini@ucr.ac.cr>
2025-06-28 18:53:29 -0600

    Update ArchivoMascotas.java

--- ArchivoMascotas.java
+++ ArchivoMascotas.java
@@ -1,3 +1,11 @@
+import java.io.PrintWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import javax.swing.JOptionPane;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+
 public class ArchivoMascotas {
     public static void guardarMascotas(ArbolMascotas arbol, String nombreArchivo) {
         try (PrintWriter writer = new PrintWriter(new FileWriter(nombreArchivo))) {
@@ -11,7 +19,7 @@ public class ArchivoMascotas {
         if (nodo != null) {
             guardarRecursivo(nodo.getIzquierdo(), writer);
             Mascota m = nodo.getDato();
-            writer.println(m.getId() + "," + m.getNombre() + "," + m.getEspecie() + "," + m.getDuenio());
+            writer.println(m.getId() + "," + m.getNombre() + "," + m.getEspecie() + "," + m.getDueno());
             guardarRecursivo(nodo.getDerecho(), writer);
         }
     }
